4eabe2a890977368c801616183c59aa151b11c83
ricks18 <77172928+ricks18@users.noreply.github.com>
2021-05-11 18:43:22 -0300

    Add files via upload
    Aqui, mostra a hora exata do seu dia

---
+++ ex014/script.js
@@ -0,0 +1,25 @@
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    // var hora = 16
+    var minuto = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${minuto} minutos.`
+    if (hora >= 0 && hora < 12) {
+        // BOM DIA
+        img.src = 'fotomanha.png'
+        document.body.style.background = '#e2cd9f'
+    } else if (hora >= 12 && hora < 18) {
+        // BOA TARDE
+        img.src = 'fototarde.png'
+        document.body.style.background = '#b9846f'
+    } else {
+        // BOA NOITE
+        img.src = 'fotonoite.png'
+        document.body.style.background = '#515154'
+    }
+}
+
+
